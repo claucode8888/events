db048aa43cc8382569c104d9adc4915aa796b2cd
Claudio Gandolffi <claucode88@gmail.com>
2026-01-19 11:14:43 +0100

    Ticket selection: layout improved.

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -11,19 +11,28 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/events')]
 final class EventController extends AbstractController
 {
-    #[Route(name: 'app_event_index', methods: ['GET'])]
-    public function index(EventRepository $eventRepository): Response
-    {
-        return $this->render('event/index.html.twig', [
-            'events' => $eventRepository->findAll(),
-        ]);
-    }
+  #[Route(name: 'app_event_index', methods: ['GET'])]
+  public function index(EventRepository $eventRepository): Response
+  {
+    return $this->render('event/index.html.twig', [
+      'events' => $eventRepository->findAll(),
+    ]);
+  }
 
-    #[Route('/{id}', name: 'app_event_show', methods: ['GET'])]
-    public function show(Event $event): Response
-    {
-        return $this->render('event/show.html.twig', [
-            'event' => $event,
-        ]);
-    }
+  #[Route('/{id}', name: 'app_event_show', methods: ['GET'])]
+  public function show(Event $event): Response
+  {
+    return $this->render('event/show.html.twig', [
+      'event' => $event,
+    ]);
+  }
+
+  //_______________________________________________________________________
+  #[Route('/buy-ticket/{id}', name: 'app_buy_ticket', methods: ['GET'])]
+  public function buyTicket(Event $event): Response
+  {
+    return $this->render('event/buy_ticket.html.twig', [
+      'event' => $event,
+    ]);
+  }
 }
