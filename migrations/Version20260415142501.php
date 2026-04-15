00b0a0db1ab9ea662ff6a576e221f521f023d1cd
Claudio Gandolffi <claucode88@gmail.com>
2026-04-15 16:26:08 +0200

    Migrations done.

---
+++ migrations/Version20260415142501.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260415142501 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA76BF700BD FOREIGN KEY (status_id) REFERENCES event_status (id)');
+        $this->addSql('CREATE INDEX IDX_3BAE0AA76BF700BD ON event (status_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA76BF700BD');
+        $this->addSql('DROP INDEX IDX_3BAE0AA76BF700BD ON event');
+    }
+}
