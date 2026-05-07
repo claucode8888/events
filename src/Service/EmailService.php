93b8837c0583bdccd5c3003cad641f281f1844ca
Claudio Gandolffi <claucode88@gmail.com>
2026-05-07 12:45:58 +0200

    Fix (mailer): services has been moved to google mailer.

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -4,22 +4,26 @@ namespace App\Service;
 
 use App\Entity\Booking;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 
 class EmailService
 {
-  public function __construct(private MailerInterface $mailer){}
+  public function __construct(
+    private MailerInterface $mailer,
+    #[Autowire('%app_sender_email%')] private string $senderEmail,
+    #[Autowire('%app_name%')] private string $appName
+  ){}
 
-
-  public function sendBookingConfirmation(Booking $booking, int $totalTickets, array $ticketsByCategory): void
+    public function sendBookingConfirmation(Booking $booking, int $totalTickets, array $ticketsByCategory): void
   {
     $eventName = $booking->getTickets()[0]->getCategory()->getEvent()->getName();
     $subject = 'Booking Confirmation '.$eventName;
 
     $email = new TemplatedEmail()
-      ->from(new Address('no-replay@eventhub.com', 'EventHub'))
-      ->to('claucode88@gmail.com')
+      ->from(new Address($this->senderEmail, $this->appName))
+      ->to($this->senderEmail)
       ->subject($subject)
       ->htmlTemplate('email/booking_confirmation.html.twig')
       ->context([
