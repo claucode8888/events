10827b02631259b00a15300840ef2647d55f9f25
Claudio Gandolffi <claucode88@gmail.com>
2026-05-13 16:29:36 +0200

    New (Stripe payment service): calling api and processing payments properly.

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\BookingRepository;
 use App\Repository\TicketRepository;
 use App\Service\EmailService;
+use App\Service\StripePaymentService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,11 +16,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PaymentController extends AbstractController
 {
   #[Route('/process', name: 'app_payment_process', methods: ['POST'])]
-  public function processPayment(
+  public function process(
     Request $request,
     BookingRepository $bookingRepository,
     TicketRepository $ticketRepository,
-    EmailService $emailService
+    EmailService $emailService,
+    StripePaymentService $stripePaymentService,
   ) : JsonResponse
   {
     $sentData = json_decode($request->getContent(), true);
@@ -33,28 +35,41 @@ final class PaymentController extends AbstractController
       ], Response::HTTP_NOT_FOUND);
     }
 
-    // Call to Payment Service 
-    $paymentResult = true;
+    // Call to Payment Service
+    $stripeSession = $stripePaymentService->createPaymentSession($booking);
 
-    if(!$paymentResult){
-      return new JsonResponse([
-        'message' => 'Payment could not be done.',
-        'success' => false
-      ]);
-    }
+    // $paymentResult = true;
+
+    // if(!$paymentResult){
+    //   return new JsonResponse([
+    //     'message' => 'Payment could not be done.',
+    //     'success' => false
+    //   ]);
+    // }
 
     // Send email confirmation
-    $queryResults = $ticketRepository->getTicketsByCategory($booking);
-    $emailSent = $emailService->sendBookingConfirmation($booking, $queryResults['total_tickets'], $queryResults['tickets_by_category']);
-    if($emailSent){
-      $this->addFlash('success', 'Booking confirmed! We\'ve emailed your tickets.');
-    }else{
-      $this->addFlash('warning', 'Payment processed, but we couldn\'t send the confirmation email. Please check your booking in "My Tickets" or contact support.');
-    }
+    // $queryResults = $ticketRepository->getTicketsByCategory($booking);
+    // $emailSent = $emailService->sendBookingConfirmation($booking, $queryResults['total_tickets'], $queryResults['tickets_by_category']);
+    // if($emailSent){
+    //   $this->addFlash('success', 'Booking confirmed! We\'ve emailed your tickets.');
+    // }else{
+    //   $this->addFlash('warning', 'Payment processed, but we couldn\'t send the confirmation email. Please check your booking in "My Tickets" or contact support.');
+    // }
 
     return new JsonResponse([
       'message' => 'Payment was successfuly.',
-      'success' => true
+      'success' => true,
+      'redirect' => $stripeSession->url
     ]);
+  }
+
+  #[Route('/success', name: 'app_payment_success')]
+  public function success(){
+    return $this->render('');
+  }
+
+  #[Route('/cancel', name: 'app_payment_cancel')]
+  public function cancel(){
+    return $this->render('');
   }
 }
---
+++ src/Service/StripePaymentService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Booking;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class StripePaymentService
+{
+  public function __construct(
+    private string $stripeSecretKey,
+    private UrlGeneratorInterface $urlGenerator,
+  ) {
+    Stripe::setApiKey($this->stripeSecretKey);
+  }
+
+  public function createPaymentSession(Booking $booking): Session
+  {
+    return Session::create([
+      'payment_method_types' => ['card'],
+      'line_items' => [[
+        'price_data' => [
+          'currency' => 'eur',
+          'product_data' => [
+            'name' => $booking->getTickets()->first()->getCategory()->getEvent()->getName(),
+          ],
+          'unit_amount' => $booking->getTotal() * 100,
+        ],
+        'quantity' => 1,
+      ]],
+      'mode' => 'payment',
+      'success_url' => $this->urlGenerator->generate('app_payment_success', [
+        'booking' => $booking->getId(),
+      ], UrlGeneratorInterface::ABSOLUTE_URL),
+      'cancel_url' => $this->urlGenerator->generate('app_payment_cancel', [
+        'booking' => $booking->getId(),
+      ], UrlGeneratorInterface::ABSOLUTE_URL),
+      'metadata' => [
+        'booking_id' => $booking->getId(),
+      ],
+    ]);
+  }
+}
