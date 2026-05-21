6577832b0b57b823fc0c55e224667b1404fe4d5a
Claudio Gandolffi <claucode88@gmail.com>
2026-05-21 15:02:05 +0200

    Testing (worker script): testing production behaviour.

--- worker.sh
+++ worker.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 sleep 8
-php bin/console messenger:consume async --memory-limit=128M --time-limit=120
+
+first_time=true
+
+while true
+
+do
+  if [ "$first_time" = false ]
+    then
+    echo " ******************************* Restarting worker... ****************************************** "
+  fi
+
+  php bin/console messenger:consume async \
+  --memory-limit=128M \
+  --time-limit=120 \
+  --no-interaction
+
+  first_time=false
+  sleep 2
+
+done
