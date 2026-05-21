587266ffec0c2190494f505a90f0e3328257a703
Claudio Gandolffi <claucode88@gmail.com>
2026-05-21 15:34:28 +0200

    Fix (worker script, messenger.yml): worker has been fixed in order to be automatically restarted as well as retry_strategy improved.

--- worker.sh
+++ worker.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 sleep 8
 
-first_time=true
-
 while true
 
 do
-  if [ "$first_time" = false ]
-    then
-    echo " ******************************* Restarting worker... ****************************************** "
-  fi
 
   php bin/console messenger:consume async \
   --memory-limit=128M \
   --time-limit=120 \
   --no-interaction
 
-  first_time=false
   sleep 2
 
 done
